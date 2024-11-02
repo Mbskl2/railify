244ece299a047fcb181092b1fbda4342c0c85b8a
Michal.Banach <michal.banach@ovivowater.com>
2024-11-02 15:27:42 +0100

    Show the image after runnning pipelines.py.

--- backend/classical_image_recognition/pipline.py
+++ backend/classical_image_recognition/pipline.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
-from backend.utils.file_to_img import pdf_to_images
+# from backend.utils.file_to_img import pdf_to_images
+import os 
+print(os.getcwd())
 
 # Setup pipline to read in Image and Extract Lines
 def read_in_image(img_path):
@@ -23,3 +25,7 @@ def read_in_image(img_path):
 if __name__ == '__main__':
     image_path = 'backend/images/L1_4711_Tobel_page_1.png'
     img = read_in_image(image_path)
+    print(img)
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
